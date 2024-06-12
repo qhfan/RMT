ae7b0cc56dd5f0a4a62efec83b3a94a71907a9f2
qhfan <67534736+qhfan@users.noreply.github.com>
2024-06-12 16:47:09 +0800

    Add files via upload

---
+++ detect/install.sh
@@ -0,0 +1,9 @@
+pip install openmim
+mim install mmengine
+pip install mmcv-full
+pip install cython==0.29.33
+pip uninstall pycocotools
+pip install mmpycocotools
+sudo python3 setup.py install
+pip install terminaltables
+pip install efficientnet_pytorch
